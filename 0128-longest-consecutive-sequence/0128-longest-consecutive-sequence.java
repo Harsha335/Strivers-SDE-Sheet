478c72c4b72d208caa193f829e80441f5b382a81
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-06-17 13:05:12 +0530

    Time: 61 ms (50.12%), Space: 64.7 MB (5.11%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,8 +4,9 @@ class Solution {
         if(cur==parent[cur]){
             return cur;
         }
-        return getparent(parent[cur]);
+        return parent[cur]=getparent(parent[cur]);
     }
+
     public int longestConsecutive(int[] nums) {
         //OR TAKE A ELEMENT AND FIND ITS PARENT THEN COUNT ELEMENTS FROM PARENT ALSO MAKE THEN VISITED REPEATE UNTIL ALL ELEMENTS ARE VISITED
         int n=nums.length;
@@ -20,42 +21,32 @@ class Solution {
         for(int i=0;i<n;i++){
             index.put(nums[i],i);
         }
-        // System.out.println(index);
         int ind;
         int maxi=1;
         for(int i=0;i<n;i++){
             int parenty=-1;
             if(index.get(nums[i])!=i)continue;
             int num=nums[i];
-            // System.out.println(index.containsKey(num+1));
             if(index.containsKey(num+1)){
                 ind=index.get(num+1);
-                if(parent[ind]!=i)
+                if(parent[ind]==ind) //ind is initail add ro num
                 {
                     parenty=getparent(i);
-                    // System.out.println(ind+" "+parenty);
                     parent[ind]=i;
                     rank[parenty]+=rank[ind];
                 }
             }
-            // System.out.println((num-1)+" "+index.containsKey(num-1));
             if(index.containsKey(num-1)){
                 ind=index.get(num-1);
-                if(parent[i]!=ind)
+                if(parent[i]==i) // i is initail add to num-1
                 {
                     parenty=getparent(ind);
                     parent[i]=ind;
                     rank[parenty]+=rank[i];
-                    // System.out.println(parenty+" "+rank[parenty]);
                 }
             }
             if(parenty!=-1)
                 maxi=Math.max(maxi,rank[parenty]);
-            // System.out.println(i+" "+parent[i]+" "+rank[i]);
-            // for(int k=0;k<n;k++){
-            //     System.out.println(k+" "+parent[k]+" "+rank[k]);
-            // }
-            // System.out.println();
         }
         return maxi;
     }

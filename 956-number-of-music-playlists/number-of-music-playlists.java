e04b0fe7fe886bb0fe2258c8f8c8010fec67965b
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-08-06 16:47:17 +0530

    Time: 1 ms (98.90%) | Memory: 41.4 MB (50.55%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.java
+++ 956-number-of-music-playlists/number-of-music-playlists.java
@@ -1,24 +1,33 @@
 class Solution {
     int mod=(int)(1e9)+7;
     long dp[][];
-    long helper(int unique,int size,int k,int total_n)
+    int n;
+    int goal;
+    int k;
+    long helper(int usedSongs,int ind)
     {
-        if(size==0 && unique==0)
+        if(usedSongs==n && ind==goal)
         {
             return 1;
         }
-        if(size==0 || unique==0) return 0;
-        if(dp[unique][size]!=-1) return dp[unique][size];
+        if(ind==goal)
+            return 0;
+        if(dp[usedSongs][ind]!=-1) return dp[usedSongs][ind];
         //taking new
-        long ans=((total_n-unique+1)*(helper(unique-1,size-1,k,total_n)))%mod;
+        long ans=0;
+        if(usedSongs<n)
+            ans=((n-usedSongs)*(helper(usedSongs+1,ind+1)))%mod;
         //old
-        if(unique>k)
-            ans=(ans+((unique-k)*(helper(unique,size-1,k,total_n)))%mod)%mod;
-        return dp[unique][size]=ans;
+        if(usedSongs>k)
+            ans=(ans+((usedSongs-k)*(helper(usedSongs,ind+1)))%mod)%mod;
+        return dp[usedSongs][ind]=ans;
     }
     public int numMusicPlaylists(int n, int goal, int k) {
-        dp=new long[n+1][goal+1];
+        dp=new long[n+1][goal];
         for(long[] x:dp) Arrays.fill(x,-1);
-        return (int)helper(n,goal,k,n)%mod;
+        this.n=n;
+        this.goal=goal;
+        this.k=k;
+        return (int)helper(0,0)%mod;
     }
 }

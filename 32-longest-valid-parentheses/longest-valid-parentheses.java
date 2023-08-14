6c43a6acae826ba4ce749fe66f0165210276621c
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-08-14 13:41:48 +0530

    Time: 4 ms (61.36%) | Memory: 41.8 MB (71.28%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,64 +1,65 @@
 class Solution {
-    // int helper(String s,Stack<Integer> stack)
-    // {
-    //     int count=0;
-    //     while(stack.size()>=2)
-    //     {
-    //         if(stack.poll)
+    // public boolean isNumeric(String strNum) {
+    //     try {
+    //         double d = Double.parseDouble(strNum);
+    //     } catch (NumberFormatException nfe) {
+    //         return false;
     //     }
+    //     return true;
     // }
-    public boolean isNumeric(String strNum) {
-        try {
-            double d = Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
     public int longestValidParentheses(String s) {
         int n=s.length();
         int maxi=0;
-        Stack<String> stack=new Stack<String>();//index
+        // Stack<String> stack=new Stack<String>();
+        // for(int i=0;i<n;i++)
+        // {
+        //     // System.out.println(i+" "+stack);
+        //     if(s.charAt(i)==')')
+        //     {
+        //         int cur=0;
+        //         while(!stack.isEmpty() && isNumeric(stack.peek()))
+        //         {
+        //             cur+=Integer.parseInt(stack.pop());
+        //         }
+        //         if(!stack.isEmpty() && stack.peek()=="(")
+        //         {
+        //             stack.pop();
+        //             while(!stack.isEmpty() && isNumeric(stack.peek()))
+        //             {
+        //                 cur+=Integer.parseInt(stack.pop());
+        //             }
+        //             maxi=Math.max(maxi,cur+2);
+        //             stack.push((cur+2)+"");
+        //         }
+        //         else{
+        //             // stack.push(cur+"");
+        //             maxi=Math.max(maxi,cur);
+        //             // stack.push(")");
+        //             stack.clear();
+        //         }
+        //     }
+        //     else
+        //         stack.push("(");
+        // }
+        // return maxi;
+        Stack<Integer> stack=new Stack<>();//index
         for(int i=0;i<n;i++)
         {
-            // System.out.println(i+" "+stack);
             if(s.charAt(i)==')')
             {
-                int cur=0;
-                while(!stack.isEmpty() && isNumeric(stack.peek()))
-                {
-                    cur+=Integer.parseInt(stack.pop());
-                }
-                if(!stack.isEmpty() && stack.peek()=="(")
+                if(!stack.isEmpty() && s.charAt(stack.peek())=='(')
                 {
                     stack.pop();
-                    while(!stack.isEmpty() && isNumeric(stack.peek()))
-                    {
-                        cur+=Integer.parseInt(stack.pop());
-                    }
-                    maxi=Math.max(maxi,cur+2);
-                    stack.push((cur+2)+"");
                 }
                 else{
-                    // stack.push(cur+"");
-                    maxi=Math.max(maxi,cur);
-                    // stack.push(")");
-                    stack.clear();
+                    stack.push(i);
                 }
+                maxi=Math.max(maxi,i-(stack.isEmpty()?-1:stack.peek()));
             }
-            else
-                stack.push("(");
+            else{
+                stack.push(i);
+            }
         }
-        // // int cur=0;
-        // // System.out.println(stack);
-        // while(!stack.isEmpty())
-        // {
-        //     if(stack.peek()==")" || stack.peek()=="(") stack.pop();
-        //     int cur=0;
-        //     while(!stack.isEmpty() && isNumeric(stack.peek()))
-        //         cur+=Integer.parseInt(stack.pop());
-        //     maxi=Math.max(cur,maxi);
-        // }
         return maxi;
     }
 }

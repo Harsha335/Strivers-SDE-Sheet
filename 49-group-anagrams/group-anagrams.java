6c250c50a5c2998d570318d7ebcf38f8bc5f8376
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-08-14 17:52:21 +0530

    Time: 25 ms (14.45%) | Memory: 53.3 MB (5.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,47 +1,15 @@
 class Solution {
-    // class Pair {
-    //     char c;
-    //     int times;
-    //     List<Trie> children;
-
-    //     Pair(char c, int times) {
-    //         this.c = c;
-    //         this.times = times;
-    //         children = new ArrayList<>();
-    //     }
-
-    //     @Override
-    //     public int hashCode() {
-    //         return Objects.hash(c, times);
-    //     }
-
-    //     @Override
-    //     public boolean equals(Object obj) {
-    //         if (this == obj) return true;
-    //         if (obj == null || getClass() != obj.getClass()) return false;
-    //         Pair pair = (Pair) obj;
-    //         return c == pair.c && times == pair.times;
-    //     }
-    // }
-
     class Trie {
-        // List<Pair> ele;
         char c;
         int times;
-        List<Trie> children; 
+        Map<Pair, Trie> children;
         List<String> list;
 
-        // Trie(Pair x) {
-        //     ele = new ArrayList<>();
-        //     ele.add(x);
-        //     list = new ArrayList<>();
-        // }
-        Trie(char c,int times)
-        {
-            this.c=c;
-            this.times=times;
-            children=new ArrayList<>();
-            list=new ArrayList<>();
+        Trie(char c, int times) {
+            this.c = c;
+            this.times = times;
+            children = new HashMap<>();
+            list = new ArrayList<>();
         }
     }
 
@@ -54,57 +22,32 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int n = strs.length;
         List<List<String>> output = new ArrayList<>();
-        Trie tree =new Trie('.',0);
-        
+        Trie tree = new Trie('.', 0);
+
         for (int i = 0; i < n; i++) {
             TreeMap<Character, Integer> counter = new TreeMap<>();
             getCounter(strs[i], counter);
             Trie ptrT = tree;
+
             for (char ch : counter.keySet()) {
                 int times = counter.get(ch);
-                boolean isFound=false;
-                for(Trie x:ptrT.children)
-                {
-                    if(x.c==ch && x.times==times)
-                    {
-                        ptrT=x;
-                        isFound=true;
-                        break;
-                    }   
+                
+                Pair pair = new Pair(ch, times);
+                if (!ptrT.children.containsKey(pair)) {
+                    Trie node = new Trie(ch, times);
+                    ptrT.children.put(pair, node);
                 }
-                if(!isFound)
-                {
-                    Trie node=new Trie(ch,times);
-                    ptrT.children.add(node);
-                    ptrT=node;
-                }
-                    // boolean isFound = false;
-                    // List<Pair> pairs = ptrT.ele;
-                    // Pair pair = new Pair(ch, times);
-                    
-                    // for (Pair p : pairs) {
-                    //     if (p.equals(pair)) {
-                    //         isFound = true;
-                    //         ptrT = p.children.get(0); // Assuming each pair has only one child in your structure
-                    //         break;
-                    //     }
-                    // }
-                    
-                    // if (!isFound) {
-                    //     Pair newPair = new Pair(ch, times);
-                    //     Trie newTrie = new Trie(newPair);
-                    //     pairs.add(newPair);
-                    //     ptrT = newTrie;
-                    // }
+
+                ptrT = ptrT.children.get(pair);
             }
-            
+
             if (ptrT.list.isEmpty()) {
                 output.add(ptrT.list);
             }
-            
+
             ptrT.list.add(strs[i]);
         }
-        
+
         return output;
     }
 }

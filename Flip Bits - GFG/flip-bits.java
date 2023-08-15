200cc2648eb15db45113bb8afe319e953ceedd9a
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-08-15 13:47:56 +0530

    Added solution - LeetHub

---
+++ Flip Bits - GFG/flip-bits.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxOnes(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static int maxOnes(int a[], int n) {
+        // Your code goes here
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==1)
+            {
+                a[i]=-1;
+                count++;
+            }
+            else a[i]=1;
+        }
+        int curSum=0;
+        int originalOnes=count;
+        for(int x:a)
+        {
+            // if(x==-1) preOne++;
+            curSum+=x;
+            if(curSum<0){
+                curSum=0;
+            }
+            // System.out.println(x+" "+curSum+" "+preOne+" "+notInclude+" "+(curSum+(nextOne-preOne)));
+            count=Math.max(count,curSum+originalOnes);
+        }
+        return count;
+    }
+}

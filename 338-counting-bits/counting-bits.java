61889d304230c8d6a61ca05007ba379464d6f662
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-09-01 22:13:09 +0530

    Time: 2 ms (73.73%) | Memory: 46.6 MB (37.75%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,21 +1,36 @@
 class Solution {
-    int findBits(int num)
-    {
-        int count=0;
-        while(num>0)
-        {
-            if(num%2!=0)
-                count++;
-            num>>=1;
-        }
-        return count;
-    }
+    // int findBits(int num)
+    // {   // Nlog(num)
+    //     int count=0;
+    //     while(num>0)
+    //     {
+    //         if(num%2!=0)
+    //             count++;
+    //         num>>=1;
+    //     }
+    //     return count;
+    // }
     public int[] countBits(int n) {
-        int output[]=new int[n+1];
-        for(int i=0;i<=n;i++)
+        // int output[]=new int[n+1];
+        // for(int i=0;i<=n;i++)
+        // {
+        //     output[i]=findBits(i);
+        // }
+        // return output;
+
+        int dp[]=new int[n+1];
+        int start=0;
+        int end=1;
+        while(end<=n)
         {
-            output[i]=findBits(i);
+            while(start<end && start+end<=n)
+            {
+                dp[start+end]=dp[start]+1;
+                start++;
+            }
+            start=0;
+            end<<=1;
         }
-        return output;
+        return dp;
     }
 }

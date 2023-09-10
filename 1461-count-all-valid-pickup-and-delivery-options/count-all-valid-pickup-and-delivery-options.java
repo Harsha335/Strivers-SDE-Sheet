3f70d0e9a00c6d057a1c254abac74104722871eb
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-09-10 12:53:56 +0530

    Time: 16 ms (21.95%) | Memory: 44 MB (21.34%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -4,28 +4,28 @@ class Solution {
     long helper(int i,int j,int n)
     {
         if(i==0 && j==0)return 1;
-        if(map[i][j]!=-1)
+        if(map[i][j]!=0)
             return map[i][j];
         long ans=0;
         //deliver
         if((n-i)>(n-j))
         {
-            ans=((j-i)*(helper(i,j-1,n)%mod))%mod;
+            ans=(j-i)*(helper(i,j-1,n));
         }
         ans=(ans%mod);
         //pickup
         if(i>0)
         {
-            ans=(ans+(i*(helper(i-1,j,n)%mod))%mod)%mod;
+            ans+=i*(helper(i-1,j,n));
         }
         return map[i][j]=(ans%mod);
     }
     public int countOrders(int n) {
         map=new long[n+1][n+1];
-        for(long[] x:map)
-        {
-            Arrays.fill(x,-1);
-        }
+        // for(long[] x:map)
+        // {
+        //     Arrays.fill(x,-1);
+        // }
         return (int)helper(n,n,n);
     }
 }

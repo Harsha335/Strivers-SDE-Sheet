73ae6b281d7cb2aff2991c93ed4c7650e2afaff8
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-09-24 11:32:30 +0530

    Time: 46 ms (50.00%) | Memory: 43.3 MB (100.00%) - LeetSync

--- 3114-beautiful-towers-i/beautiful-towers-i.java
+++ 3114-beautiful-towers-i/beautiful-towers-i.java
@@ -1,40 +1,31 @@
 class Solution {
     public long maximumSumOfHeights(List<Integer> maxHeights) {
-        // int maxi=0;
-        // int index=0;
-        // int ind=0;
-        // for(int x:maxHeight)
-        // {
-        //     if(maxi<x)
-        //     {
-        //         maxi=x;
-        //         index=ind;
-        //     }
-        //     ind++;
-        // }
-        double maxsum=0;
+        long maxsum=0;
         int n=maxHeights.size();
-        // int pre=maxi;
         for(int index=0;index<n;index++)
         {
             // System.out.println(index);
-            List<Integer> maxHeight=new ArrayList(maxHeights);
-            double sum=0;
+            // List<Integer> maxHeight=new ArrayList(maxHeights);
+            long sum=maxHeights.get(index);
+            int curMaxi=maxHeights.get(index);
             for(int i=index-1;i>=0;i--)
             {
-                maxHeight.set(i,Math.min(maxHeight.get(i),maxHeight.get(i+1)));
-                sum+=maxHeight.get(i);
+                // maxHeight.set(i,Math.min(maxHeight.get(i),maxHeight.get(i+1)));
+                curMaxi=Math.min(curMaxi,maxHeights.get(i));
+                sum+=curMaxi;
             }
+            curMaxi=maxHeights.get(index);
             for(int i=index+1;i<n;i++)
             {
                 
-                maxHeight.set(i,Math.min(maxHeight.get(i),maxHeight.get(i-1)));
-                sum+=maxHeight.get(i);
+                // maxHeight.set(i,Math.min(maxHeight.get(i),maxHeight.get(i-1)));
+                curMaxi=Math.min(curMaxi,maxHeights.get(i));
+                sum+=curMaxi;
             }
-            maxsum=Math.max(maxsum,sum+(double)maxHeight.get(index));
+            maxsum=Math.max(maxsum,sum);
             // System.out.println(maxsum);
         }
         
-        return (long)maxsum;
+        return maxsum;
     }
 }

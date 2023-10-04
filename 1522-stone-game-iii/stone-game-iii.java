c68a2cfe5fb0e355cea3e656023f89b1db9df9fb
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-10-04 15:55:34 +0530

    Time: 35 ms (51.67%) | Memory: 61.3 MB (58.37%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,47 +1,46 @@
 class Solution {
     int n;
-    int dp[][];
-    int helper(int[] stones,boolean isAlice,int ind)
+    int dp[];
+    int helper(int[] stones,int ind)
     {
         if(ind==n)
             return 0;
-        if(dp[ind][isAlice?1:0]!=-1)
-            return dp[ind][isAlice?1:0];
-        if(isAlice)
-        {
+        if(dp[ind]!=-1)
+            return dp[ind];
+        // if(isAlice)
+        // {
             int res=-(int)1e9;
             int value=0;
             for(int i=ind;i<n && i<ind+3;i++)
             {
                 value+=stones[i];
-                res=Math.max(res,helper(stones,false,i+1)+value);
+                res=Math.max(res,value-helper(stones,i+1));
             }
-            return dp[ind][isAlice?1:0]=res;
-        }
-        else{
-            int res=(int)1e9;
-            // int value=0;
-            for(int i=ind;i<n && i<ind+3;i++)
-            {
-                // value+=stones[i];
-                res=Math.min(res,helper(stones,true,i+1));
-            }
-            return dp[ind][isAlice?1:0]=res;
-        }
+            return dp[ind]=res;
+        // }
+        // else{
+        //     int res=(int)1e9;
+        //     // int value=0;
+        //     for(int i=ind;i<n && i<ind+3;i++)
+        //     {
+        //         // value+=stones[i];
+        //         res=Math.min(res,helper(stones,true,i+1));
+        //     }
+        //     return dp[ind][isAlice?1:0]=res;
+        // }
     }
     public String stoneGameIII(int[] stoneValue) {
         n=stoneValue.length;
-        int total=0;
-        for(int x:stoneValue)
-            total+=x;
-        dp=new int[n][2];
-        for(int x[]:dp)
-            Arrays.fill(x,-1);
-        int res=helper(stoneValue,true,0);
+        // int total=0;
+        // for(int x:stoneValue)
+        //     total+=x;
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+        int res=helper(stoneValue,0);
         // System.out.println(res);
-        if((double)res==(double)total/2.0)
+        if(res==0)
             return "Tie";
-        else if((double)res>(double)total/2.0)
+        else if(res>0)
             return "Alice";
         return "Bob";
     }

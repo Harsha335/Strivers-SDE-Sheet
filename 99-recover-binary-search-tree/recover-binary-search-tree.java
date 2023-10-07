4559d263d91ca12a20bd82c11018537d65a014e1
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-10-07 11:42:36 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (42.58%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,46 +14,57 @@
  * }
  */
 class Solution {
-    HashMap<Integer,TreeNode> map;
-    List<Integer> arr;
+    // HashMap<Integer,TreeNode> map;
+    // List<Integer> arr;
+    TreeNode first=null;
+    TreeNode last=null;
+    TreeNode prev=new TreeNode(Integer.MIN_VALUE);
     void dfs(TreeNode root)
     {
         if(root!=null)
         {
-            map.put(root.val,root);
+            // map.put(root.val,root);
             dfs(root.left);
-            arr.add(root.val);
+            // arr.add(root.val);
+            if(first==null && root.val<prev.val)
+                first=prev;
+            if(first!=null && root.val<prev.val)
+                last=root;
+            prev=root;
             dfs(root.right);
         }
     }
     public void recoverTree(TreeNode root) {
-        map=new HashMap<>();
-        arr=new ArrayList<>();
+        // map=new HashMap<>();
+        // arr=new ArrayList<>();
         dfs(root);
-        // List<Integer> sorted=new ArrayList<>(arr);
-        // Collections.sort(sorted);
-        int n=arr.size();
-        int first=-1;
-        int last=-1;
-        boolean isFirst=true;
-        for(int i=0;i<n-1;i++)
-        {
-            if(arr.get(i)>arr.get(i+1))
-            {
-                if(isFirst)
-                {
-                    first=arr.get(i);
-                    last=arr.get(i+1);
-                    isFirst=false;
-                }
-                else{
-                    last=arr.get(i+1);
-                }
-            }
-        }
-        if(first==last && first==-1)
-            return;
-        map.get(first).val=last;
-        map.get(last).val=first;
+        int value=first.val;
+        first.val=last.val;
+        last.val=value;
+        // // List<Integer> sorted=new ArrayList<>(arr);
+        // // Collections.sort(sorted);
+        // int n=arr.size();
+        // int first=-1;
+        // int last=-1;
+        // boolean isFirst=true;
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     if(arr.get(i)>arr.get(i+1))
+        //     {
+        //         if(isFirst)
+        //         {
+        //             first=arr.get(i);
+        //             last=arr.get(i+1);
+        //             isFirst=false;
+        //         }
+        //         else{
+        //             last=arr.get(i+1);
+        //         }
+        //     }
+        // }
+        // if(first==last && first==-1)
+        //     return;
+        // map.get(first).val=last;
+        // map.get(last).val=first;
     }
 }

76f9a3ea590a34d6b6392f071141e06bab808711
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-10-15 11:56:58 +0530

    Time: 11 ms (56.10%) | Memory: 43 MB (72.56%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,8 +1,7 @@
 class Solution {
     private int dir[]=new int[]{-1,0,1};
     private int mod=(int)1e9+7;
-    // private int dp[][];
-    HashMap<List<Integer>,Integer> dp;
+    private int dp[][];
     private int helper(int pos,int steps,int arrLen)
     {
         if(pos<1 || pos>arrLen)
@@ -13,28 +12,22 @@ class Solution {
                 return 1;
             return 0;
         }
-        List<Integer> list=Arrays.asList(pos,steps);
-        if(dp.containsKey(list))
-            return dp.get(list);
-        // if(dp[pos][steps]!=-1)
-        //     return dp[pos][steps];
+        if(dp[pos][steps]!=-1)
+            return dp[pos][steps];
         int ans=0;
         for(int d:dir)
         {
             ans=(ans+helper(pos+d,steps-1,arrLen))%mod;
         }
-        // dp[pos][steps]=
-        dp.put(list,ans);
-        return ans;
+        return dp[pos][steps]=ans;
     }
     public int numWays(int steps, int arrLen) {
-        // dp=new int[arrLen+1][steps+1];
-        // for(int x[]:dp)
-        // {
-        //     Arrays.fill(x,-1);
-        // }
-        dp=new HashMap<>();
-
+        arrLen=Math.min(arrLen,steps);//THIS MOVE\U0001f44d
+        dp=new int[arrLen+1][steps+1];
+        for(int x[]:dp)
+        {
+            Arrays.fill(x,-1);
+        }
         return helper(1,steps,arrLen);
     }
 }

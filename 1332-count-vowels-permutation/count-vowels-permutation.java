3a603eb53eb056a9ee08acdb97e6b52ba88a3be9
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-10-28 08:48:55 +0530

    Time: 34 ms (22.11%) | Memory: 47.1 MB (20.46%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -1,37 +1,38 @@
 class Solution {
     private int mod = (int)1e9 + 7;
-    HashMap<Character,List<Character>> map;
+    HashMap<Integer,List<Integer>> map;
     long dp[][];
-    private long dfs(char c,int n)
+    private long dfs(int c,int n)
     {
         if(n==0)
         {
             return 1;
         }
-        if(dp[c-'a'][n]!=-1)
-            return dp[c-'a'][n];
+        if(dp[c][n]!=-1)
+            return dp[c][n];
         long ans=0;
-        for(char x:map.get(c))
+        for(int x:map.get(c))
         {
             ans+=dfs(x,n-1);
             ans=(ans)%mod;
         }
-        return dp[c-'a'][n]=ans;
+        return dp[c][n]=ans;
     }
     public int countVowelPermutation(int n) {
+        //  a: 0, e: 1, i: 2, o: 3, u: 4
         map=new HashMap<>();
-        map.put('a',Arrays.asList('e'));
-        map.put('e',Arrays.asList('a','i'));
-        map.put('i',Arrays.asList('a','e','o','u'));
-        map.put('o',Arrays.asList('i','u'));
-        map.put('u',Arrays.asList('a'));
+        map.put(0,Arrays.asList(1));
+        map.put(1,Arrays.asList(0,2));
+        map.put(2,Arrays.asList(0,1,3,4));
+        map.put(3,Arrays.asList(2,4));
+        map.put(4,Arrays.asList(0));
         long count=0;
-        dp=new long[26][n];
+        dp=new long[5][n];
         for(long x[]:dp)
             Arrays.fill(x,-1);
-        for(char key:map.keySet())
+        for(int i=0;i<5;i++)
         {
-            count+=dfs(key,n-1);
+            count+=dfs(i,n-1);
             count=(count)%mod;
         }
         return (int)count;

e19f8403ddd365254e88d1622b7bd9af3225886a
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-11-17 21:34:11 +0530

    Time: 1765 ms (51.96%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,1 +1,2 @@
-select s1.product_id , s1.year as first_year, s2.quantity, s2.price from (select product_id,min(year) as year from Sales group by product_id) s1 join (select product_id,year,quantity,price from Sales ) s2 on s1.product_id=s2.product_id and s1.year=s2.year;
+# select s1.product_id , s1.year as first_year, s2.quantity, s2.price from (select product_id,min(year) as year from Sales group by product_id) s1 join (select product_id,year,quantity,price from Sales ) s2 on s1.product_id=s2.product_id and s1.year=s2.year;
+select product_id , year as first_year, quantity, price from sales where (product_id,year) in (select product_id,min(year) as year from Sales group by product_id);

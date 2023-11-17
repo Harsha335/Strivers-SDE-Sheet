9e17b5390c7a003c7184dea96410efc6b7172da3
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-11-17 08:31:07 +0530

    Time: 780 ms (56.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,1 @@
+select p.query_name,round(avg(rating/position),2) as quality,round((select count(*) from Queries q where (q.rating<3 and q.query_name=p.query_name))/count(*)*100,2) as poor_query_percentage from Queries p group by p.query_name;

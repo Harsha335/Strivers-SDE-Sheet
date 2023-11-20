e848aa153615d77b6bd9beb32273ea99562fcdfa
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-11-20 20:43:26 +0530

    Time: 38 ms (35.23%) | Memory: 59.3 MB (98.30%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,33 +1,28 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        HashMap<Character,Integer> counter=new HashMap<>();
-        counter.put('M',0);
-        counter.put('G',0);
-        counter.put('P',0);
         HashMap<Character,Integer> lastInd=new HashMap<>();
         lastInd.put('M',0);
         lastInd.put('G',0);
         lastInd.put('P',0);
         int n=travel.length;
-        int preSum[]=new int[n+1];
-        preSum[0]=0;
+        int[]preSum=new int[n+1];
         for(int i=0;i<n;i++)
         {
             preSum[i+1]=preSum[i]+travel[i];
         }
+        int output=0;
         for(int i=0;i<=n;i++)
         {
             String s=garbage[i];
             for(char c:s.toCharArray())
             {
-                counter.put(c,counter.get(c)+1);
+                output+=1;
                 lastInd.put(c,i);
             }
         }
-        int output=0;
-        for(char c:counter.keySet())
+        for(char c:lastInd.keySet())
         {
-            output+=preSum[lastInd.get(c)]+counter.get(c);
+            output+=preSum[lastInd.get(c)];
         }
         return output;
     }

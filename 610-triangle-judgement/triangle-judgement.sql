b9a68ae1895160c05cf1f5dd87604ce14dd4e9ce
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-11-21 23:09:35 +0530

    Time: 537 ms (32.05%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,3 +1,1 @@
-# Write your MySQL query statement below
-select x,y,z,(if((x+y+z)-greatest(x,y,z)>greatest(x,y,z),"Yes","No")) as triangle from Triangle;
-# select x,y,z,(CASE WHEN ((x+y+z)-greatest(x,y,z))>greatest(x,y,z) then "Yes" ELSE "No" END) as triangle from Triangle;
+select x,y,z,if((x+y+z)>2*greatest(x,y,z),"Yes","No") as triangle from Triangle;

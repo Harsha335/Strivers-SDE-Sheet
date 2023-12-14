27331ed4038017657ca274faab3317c4f539145f
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2023-12-14 11:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (41.10%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,11 +1,17 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        int ans = n;
-        ans ^= ans >> 16;
-        ans ^= ans >> 8;
-        ans ^= ans >> 4;
-        ans ^= ans >> 2;
-        ans ^= ans >> 1;
-        return ans;
+        int output=0;
+        int mask=1;
+        int index=0;
+        while(mask<=n)
+        {
+            if((mask&n)!=0)
+            {
+                output=((1<<(index+1))-1)-output;
+            }
+            mask<<=1;
+            index++;
+        }
+        return output;
     }
 }

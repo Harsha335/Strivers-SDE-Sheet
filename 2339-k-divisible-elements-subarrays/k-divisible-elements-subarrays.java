81e4e35271872dbfc1ab8ade4b5a66f01f82657b
Harsha335 <94928445+Harsha335@users.noreply.github.com>
2024-01-03 15:47:55 +0530

    Time: 30 ms (93.97%) | Memory: 80.9 MB (5.03%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
@@ -1,10 +1,27 @@
 class Solution {
+    class Trie{
+        Trie arr[];
+        boolean isEnd;
+        Trie(int maxLen)
+        {
+            arr = new Trie[maxLen+1];
+            isEnd = false;
+        }
+    }
     public int countDistinct(int[] nums, int k, int p) {
-        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        // HashSet<ArrayList<Integer>> set = new HashSet<>();
+        int maxi = 0;
+        for(int ele: nums)
+        {
+            maxi = Math.max(maxi,ele);
+        }
+        Trie root = new Trie(maxi);
         int n = nums.length;
+        int output = 0;
         for(int i = 0; i < n; i++)
         {
-            ArrayList<Integer> list = new ArrayList<>();
+            // ArrayList<Integer> list = new ArrayList<>();
+            Trie curr = root;
             int count = 0;
             for(int j = i; j < n; j++)
             {
@@ -12,11 +29,21 @@ class Solution {
                     count++;
                 if(count > k)
                     break;
+                if(curr.arr[nums[j]] == null)
+                {
+                    curr.arr[nums[j]] = new Trie(maxi);
+                }
+                if(curr.arr[nums[j]].isEnd == false)
+                {
+                    output ++;
+                    curr.arr[nums[j]].isEnd = true;
+                }
+                curr = curr.arr[nums[j]];
                 // System.out.println(set);
-                list.add(nums[j]);
-                set.add((ArrayList)list.clone());
+                // list.add(nums[j]);
+                // set.add((ArrayList)list.clone());
             }
         }
-        return set.size();
+        return output;
     }
 }
